a677611320e8bcca094085af419b009334aa447b
Anas Khan <anas.khan96@outlook.com>
2017-05-28 22:30:24 +0530

    first attempt at loading comics

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 'use strict';
+
+var fetch = require('node-fetch');
+
+function loadComics(callback) {
+    fetch('https://consolia-comic.com/api/comic', {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        }
+    }).then((response) => response.json())
+        .then((res) => {
+            callback(res, null);
+        }).catch((err) => {
+            callback(undefined, err);
+        });
+}
+
+module.exports = {
+    loadComics
+}
