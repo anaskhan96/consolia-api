17b3006091a99301736b8741c886984bd44cd7b7
Anas Khan <anas.khan96@outlook.com>
2017-05-29 16:56:07 +0530

    completed cli utility

--- src/cli.js
+++ src/cli.js
@@ -1,17 +1,27 @@
 #! /usr/bin/env node
 const consolia = require('../index.js');
 
+function printUsageDetails() {
+    console.log("\nUsage Details:\n");
+    console.log("consolia [ latest | random | <id> ]\n");
+    process.exit(1);
+}
+
+function printComic(action) {
+    consolia.load(action, function (data, err) {
+        if (err != null) {
+            console.log("Error occurred: " + err);
+            process.exit(1);
+        } else {
+            console.log(data);
+            process.exit(0);
+        }
+    });
+}
+
 if (process.argv.length != 3) {
     return printUsageDetails();
 } else {
     var action = process.argv[2];
-    if (isNaN(action)) {
-        if (action == 'latest')
-            printLatest();
-        else if (action == 'random')
-            printRandom();
-        else
-            return printUsageDetails();
-    } else
-        printById(action);
+    printComic(action);
 }
